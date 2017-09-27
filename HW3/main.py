d11848fd206a9b1c503888060b45a828e1830898
Derek Chiang <b00611007@gmail.com>
2017-09-26 22:07:32 -0400

    Modify the spectral clustering setting.

--- HW3/main.py
+++ HW3/main.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
 import pandas as pd
 from os.path import dirname, join
 from bokeh.io import curdoc
 from bokeh.plotting import figure
 from bokeh.models import (
-    ColumnDataSource, Slider, NumeralTickFormatter, CDSView, GroupFilter, 
-    TapTool, HoverTool, BoxSelectTool, BoxZoomTool, LassoSelectTool
+    ColumnDataSource, Slider, NumeralTickFormatter, HoverTool
 )
 from bokeh.layouts import column, row, widgetbox
 from bokeh.models.widgets import (
-    CheckboxGroup, Select, Div, Panel, Tabs,
-    DataTable, TableColumn, NumberFormatter
+    Select, Div, Panel, Tabs, NumberFormatter
 )
 from sklearn.cluster import KMeans, SpectralClustering
 
@@ -17,10 +19,20 @@ from sklearn.cluster import KMeans, SpectralClustering
 def nix(val, lst):
     return [x for x in lst if x != val]
 
-def get_data(t1, t2, t3, t4, clusters):
+def get_data(t1, t2, alg, reg, clusters):
+    """ Get the data according to user's settings.
+
+    Args:
+        t1: the selected column for x axis
+        t2: the selected column for y axis
+        alg: the clustering algorithm
+        reg: the region for filtered plot
+        clusters: the number of clusters for the clustering algorithm
+
+    """
     
     # Run K-means clustering
-    if t3 == METHODS[0]:
+    if alg == METHODS[0]:
         labels, centroids = run_Kmeans(data, clusters)
 
         # For K-means clustering, plot the centroid of each cluster
@@ -29,17 +41,17 @@ def get_data(t1, t2, t3, t4, clusters):
         y = [coord[columns.index(t2)] for coord in centroids]
         d3 = dict(index=number, x=x, y=y, color=COLORS[:clusters])
         
-    
     # Run Spectral clustering
-    elif t3 == METHODS[1]:
-        labels = run_SpectralClustering(data, clusters)
+    elif alg == METHODS[1]:
+        # Run the normalized data for the affinity matrix
+        labels = run_SpectralClustering(data_norm, clusters)
         d3 = dict(index=[], x=[], y=[], color=[])
 
     indices = list(df.index)
     colors = [COLORS[x] for x in labels]
     d1 = dict(index=indices, x=df[t1], y=df[t2], color=colors)
 
-    region_df = df.loc[lambda df: df.Region == int(t4), :]
+    region_df = df.loc[lambda df: df.Region == int(reg), :]
     region_indices = list(region_df.index)
     region_colors = [colors[x] for x in region_indices]
     d2 = dict(index=region_indices, x=region_df[t1], y=region_df[t2], color=region_colors)
@@ -59,31 +71,37 @@ def run_SpectralClustering(data, clusters):
 df = pd.read_csv(join(dirname(__file__), 'data/Wholesale customers data.csv'))
 columns = sorted(df.columns[2:])
 data = df[columns].as_matrix()
+
+# Normalize each column (for Spectral Clustering)
+df_norm = (df - df.min()) / (df.max() - df.min())
+data_norm = df_norm[columns].as_matrix()
+
+# Set the DataSource for the plots
 s1 = ColumnDataSource(data=dict(index=[], x=[], y=[], color=[]))
 s2 = ColumnDataSource(data=dict(index=[], x=[], y=[], color=[]))
 s3 = ColumnDataSource(data=dict(index=[], x=[], y=[], color=[]))
 
+
 # Default settings
 DEFAULT_TICKERS = columns
 METHODS = ['K-means', 'Spectral']
-DEFALUT_TOOLS = ['pan', 'box_zoom', 'lasso_select', 'reset']
+DEFALUT_TOOLS = ['pan', 'box_zoom', 'reset']
 COLORS = ['#ae254a', '#007380', '#4364ae', '#f9d500',
           '#ff7256', '#800080', '#0e2f44', '#f442cb']
-PLOT_PARAM = {'plot_height': 400, 'plot_width': 600,
-              'responsive': True, 'toolbar_location': 'above'}
+PLOT_PARAM = {'plot_height': 400, 'plot_width': 600, 'responsive': True,
+              'toolbar_location': 'above', 'tools': DEFALUT_TOOLS}
 
 # Draw an interactive scatter plot to show the data distribution
-p1 = figure(tools=DEFALUT_TOOLS, **PLOT_PARAM)
+p1 = figure(**PLOT_PARAM)
 p1.circle('x', 'y', source=s1, size=12, color='color', alpha=0.6)
 p1.circle_x('x', 'y', source=s3, size=20, color='color', fill_alpha=1.0, line_color='black', line_width=2)
 p1.title.text_font_size='16px'
 p1.xaxis.formatter=p1.yaxis.formatter=NumeralTickFormatter(format='$ 0,0')
 
-p2 = figure(x_range=p1.x_range, y_range=p1.y_range, tools=DEFALUT_TOOLS, **PLOT_PARAM)
+p2 = figure(x_range=p1.x_range, y_range=p1.y_range, **PLOT_PARAM)
 p2.circle('x', 'y', source=s2, size=12, color='color', alpha=0.6)
 p2.title.text_font_size='16px'
 p2.xaxis.formatter=p2.yaxis.formatter=NumeralTickFormatter(format='$ 0,0')
-
 
 # Add a hover tool to the scatter plot
 hover = HoverTool(
@@ -94,14 +112,16 @@ hover = HoverTool(
         ]
     )
 p1.add_tools(hover)
+p2.add_tools(hover)
 
 
-# Set up widget
+# Set up widgets
 x_ticker = Select(title='X Axis:', value='Milk', options=nix('Fresh', DEFAULT_TICKERS))
 y_ticker = Select(title='Y Axis:', value='Fresh', options=nix('Milk', DEFAULT_TICKERS))
 alg_ticker = Select(title='Clustering:', value='K-means', options=METHODS)
 reg_ticker = Select(title='Region:', value='1', options=['1', '2', '3'])
 cluster_slider = Slider(title='Clusters', value=4, start=1, end=8, step=1)
+
 
 # Set up callbacks
 def x_ticker_change(attrname, old, new):
