abc2aaebc0d1dc7d15ea505f7765ca47e9fa5fef
Derek Chiang <b00611007@gmail.com>
2017-10-29 02:41:39 -0400

    Add another time-series plot.

--- HW6/hashtag.py
+++ HW6/hashtag.py
@@ -10,23 +10,33 @@ from bokeh.layouts import column, row, widgetbox
 from bokeh.models.glyphs import Text
 from bokeh.models import ColumnDataSource, Slider, HoverTool, LabelSet
 from bokeh.models.widgets import Select, Div
-from scripts.utils import get_tagCount, extract_hashtags, count_cooccurence
+from scripts.utils import extract_hashtags, count_cooccurence
 
 pd.options.mode.chained_assignment = None
 
 threshold = 30
 time_unit = ['Day', 'Week', 'Month']
-p2_tools = 'pan,xbox_select,reset'
-p1_params = {'plot_height': 350, 'plot_width': 350, 'toolbar_location': None}
-p2_params = {'plot_height': 300, 'plot_width': 900,
-             'x_axis_type': 'datetime', 'active_drag': "xbox_select"}
+p1_params = {'plot_height': 400, 'plot_width': 400, 'toolbar_location': None}
+p2_params = {'plot_height': 250, 'plot_width': 800, 'x_axis_type': 'datetime',
+             'active_drag': "xbox_select", 'tools': 'pan,xbox_select,reset'}
+p3_params = {'plot_height': 250, 'plot_width': 800, 'x_axis_type': 'datetime',
+             'tools': '', 'toolbar_location': None}
 
 
-def get_data(time):
-    """ time = ['Day'|'Week'|'Month']. """
-    grouped = time_df.groupby(pd.TimeGrouper(freq=time[:1]))
-    time_dict = {time_stamp: get_tagCount(texts.tolist()) for time_stamp, texts in grouped}
-    return time_dict.keys(), time_dict.values()
+def get_data(time_unit):
+    """ time_unit = ['Day'|'Week'|'Month']. """
+    grouped = tag_table.groupby(pd.TimeGrouper(freq=time_unit[:1]))
+    time_stamps = []
+    hash_lists = []
+    for time_stamp, hash_dicts in grouped:
+        group_dict = dict()
+        for hash_dict in hash_dicts:
+            for k, v in hash_dict.items():
+                group_dict[k] = group_dict.get(k, 0) + 1
+
+        time_stamps.append(time_stamp)
+        hash_lists.append(group_dict)
+    return time_stamps, hash_lists
 
 
 # Read in dataset
@@ -34,56 +44,72 @@ fpath = './data/costco/export_dashboard_cost_2016_06_15_12_24_55.xlsx'
 df = pd.read_excel(fpath, sheetname='Stream')
 
 # Preprocess the data
-time_df = df['Tweet content']
-time_df.index = pd.to_datetime(df['Date'] + ' ' + df['Hour'])
-id2word, word2id = extract_hashtags(time_df, threshold)
-co_occurence = count_cooccurence(time_df, word2id)
-words = list(word2id.keys())
-nWords = len(words)
+tag_table = pd.Series([extract_hashtags(tweet) for tweet in df['Tweet content']])
+tag_table.index = pd.to_datetime(df['Date'] + ' ' + df['Hour'])
 
-# Set up position for the nodes
-theta = 2 * np.pi / nWords
-circ = [i * theta for i in range(nWords)]
-coord_x = [np.cos(i) for i in circ]
-coord_y = [np.sin(i) for i in circ]
-color = ['#1F77B4'] * nWords
+# Get all hashtags from tweets and index them
+top_hashtags = extract_hashtags(df['Tweet content'], threshold)
+id2word = {i: tag for i, tag in enumerate(sorted(top_hashtags))}
+word2id = {tag: i for i, tag in id2word.items()}
+co_occurence = count_cooccurence(tag_table, word2id)
+Tags = list(word2id.keys())
+nTags = len(Tags)
+colors = ['#1F77B4'] * nTags
 
+# Construct co-occurence graph and set up position for the nodes
+radius = 4
+theta = 2 * np.pi / nTags
+circ = [i * theta for i in range(nTags)]
+pos_x = [radius * np.cos(i) for i in circ]
+pos_y = [radius * np.sin(i) for i in circ]
 
 # Set up DataSource and figures
-s1 = ColumnDataSource(data=dict(x=coord_x, y=coord_y, hashtag=words, color=color))
+s1 = ColumnDataSource(data=dict(x=pos_x, y=pos_y, text=Tags, color=colors))
 s2 = ColumnDataSource(data=dict(x=[], y=[], hashtags=[]))
+s3 = ColumnDataSource(data=dict(x=[], y=[]))
+# s3 = ColumnDataSource
+hover1 = HoverTool(tooltips=[('Hashtag', '@text')])
+hover2 = HoverTool(tooltips=[('Count of Hashtags', '@y')], mode='vline')
+hover3 = HoverTool(tooltips=[('Count of Hashtags', '@y')], mode='vline')
 
-hover1 = HoverTool(tooltips=[('Hashtag', '@hashtag')])
-hover2 = HoverTool(tooltips=[('Num of Hashtag', '@y')], mode='vline')
-
-
-# Draw the edges between the nodes
+# Draw the co-occurence graph
 p1 = figure(title="Hashtag co-occurence graph", tools=[hover1], **p1_params)
 for (n1, n2), count in co_occurence.items():
-    n1_x, n1_y = coord_x[n1], coord_y[n1]
-    n2_x, n2_y = coord_x[n2], coord_y[n2]
-    p1.line([n1_x, n2_x], [n1_y, n2_y], line_color="#949494")
-scatter = p1.circle('x', 'y', source=s1, size=20)
-scatter = p1.circle('x', 'y', source=s1, color='color', size=20)
+    n1_x, n1_y = pos_x[n1], pos_y[n1]
+    n2_x, n2_y = pos_x[n2], pos_y[n2]
+
+    width = 2
+    if count > 5 and count <= 10:
+        width = 4
+    elif count > 10:
+        width = 6
+    p1.line([n1_x, n2_x], [n1_y, n2_y], line_color="#aeaeae", line_width=width)
+scatter = p1.circle('x', 'y', source=s1, color='color', size=24)
 hover1.renderers.append(scatter)
 
-p2 = figure(title="Hashtag count", tools=p2_tools, **p2_params)
+p2 = figure(title="Distribution of total hashtag count", **p2_params)
 p2.line('x', 'y', source=s2, selection_color="orange", line_width=3)
 p2.circle('x', 'y', size=6, source=s2, color=None, selection_color="orange")
 p2.add_tools(hover2)
+
+p3 = figure(title="Distribution", **p3_params)
+p3.line('x', 'y', source=s3, color='#007380', selection_color="orange", line_width=3)
+p3.circle('x', 'y', size=6, source=s3, color=None, selection_color="orange")
+p3.add_tools(hover3)
 
 # Set up figure parameters
 p1.grid.visible = False
 p1.axis.visible = False
 p1.outline_line_color = None
-p1.title.text_font_size = p2.title.text_font_size = '16px'
-
+p2.yaxis.axis_label = p3.yaxis.axis_label = 'Count'
+p1.title.text_font_size = p2.title.text_font_size = p3.title.text_font_size = '18px'
 
 # Set up widgets
-time_ticker = Select(title='Time:', value='Day', options=time_unit)
+time_ticker = Select(title='Time Unit:', value='Month', options=time_unit)
+hashtag_ticker = Select(title='Hashtag', value=Tags[0], options=Tags)
 
 
-def time_change(attrname, old, new):
+def data_change(attrname, old, new):
     update()
 
 
@@ -97,27 +123,40 @@ def selection_change(attrname, old, new):
             hashtags += data[index]
         hashtags = set(hashtags)
 
-    new_color = ['#1F77B4'] * nWords
+    new_color = ['#1F77B4'] * nTags
     for t in hashtags:
         if t in word2id.keys():
             new_color[word2id[t]] = '#ae254a'
-    s1.data = dict(x=coord_x, y=coord_y, hashtag=words, color=new_color)
+    s1.data = dict(x=pos_x, y=pos_y, text=Tags, color=new_color)
 
 
 def update():
-    t1 = time_ticker.value
+    t1, t2 = time_ticker.value, hashtag_ticker.value
 
     time_stamps, tag_dicts = get_data(t1)
     nTag_count = [sum(tag_dict.values()) for tag_dict in tag_dicts]
     nTag_list = [list(tag_dict.keys()) for tag_dict in tag_dicts]
 
-    s2.data = dict(x=list(time_stamps), y=nTag_count, hashtags=nTag_list)
+    t_count = list()
+    for tag_dict in tag_dicts:
+        if t2 in tag_dict:
+            t_count.append(tag_dict[t2])
+        else:
+            t_count.append(0)
+
+    s2.data = dict(x=time_stamps, y=nTag_count, hashtags=nTag_list)
+    s3.data = dict(x=time_stamps, y=t_count)
+
+    p2.xaxis.axis_label = p3.xaxis.axis_label = 'Date(' + t1 + ')'
+    p3.title.text = "Distribution of Hashtag: {}".format(t2)
 
 
-time_ticker.on_change('value', time_change)
+time_ticker.on_change('value', data_change)
+hashtag_ticker.on_change('value', data_change)
 s2.on_change('selected', selection_change)
 
-layout = column(p1, time_ticker, p2)
+time_widget = column(row(time_ticker, hashtag_ticker), p2, p3)
+layout = row(p1, time_widget)
 
 # Initialize
 update()
--- HW6/scripts/utils.py
+++ HW6/scripts/utils.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import re
+import pandas as pd
 from itertools import combinations
+
+hashtag_re = re.compile(r"#\w+[\w'-]*\w+")
 
 
 def checkNaN(data):
@@ -16,6 +19,7 @@ def preprocess(tweets):
     for i, t in enumerate(tweets):
         tweets[i] = preprocess_a_tweet(t)
     return tweets
+
 
 def preprocess_a_tweet(t):
     ret_str = None
@@ -32,58 +36,26 @@ def preprocess_a_tweet(t):
     return ret_str
 
 
-def get_tagCount(tweets, threshold=0):
+def extract_hashtags(tweets, threshold=0):
     """ Extract hashtags from tweets and index them. """
+
     tag_count = dict()
-    if isinstance(tweets, list):
+
+    # Check if the input is a list/Series of strings
+    if isinstance(tweets, (list, pd.Series)):
         for tweet in tweets:
-            for tag in re.findall(r"#\w+[\w'-]*\w+", tweet.lower()):
+            for tag in re.findall(hashtag_re, tweet.lower()):
                 tag_count[tag] = tag_count.get(tag, 0) + 1
     else:
-        for tag in re.findall(r"#\w+[\w'-]*\w+", tweets.lower()):
+        for tag in re.findall(hashtag_re, tweets.lower()):
             tag_count[tag] = tag_count.get(tag, 0) + 1
 
     # Extract hashtags with count higher than threshold
-    top_tags = {t: tag_count[t] for t, c in tag_count.items() if c > threshold}
+    top_tags = {t: c for t, c in tag_count.items() if c > threshold}
     return top_tags
 
 
-def extract_hashtags(tweets, threshold):
-    """ Extract hashtags from tweets and index them.
-
-    Parameters
-    ----------
-        tweets : list of strings
-            Raw tweet text.
-
-        threshold : int
-            Set the threshold for the count of hashtag. A hashtag
-            is extracted if its count is higher than the threshold.
-
-    Returns
-    -------
-        id2word : dict
-            Index the extracted hashtags.
-
-        word2id : dict
-            Provide a look-up dictionary.
-
-    """
-    hashtag_count = dict()
-    for tweet in tweets:
-        for tag in re.findall(r"#\w+[\w'-]*\w+", tweet.lower()):
-            hashtag_count[tag] = hashtag_count.get(tag, 0) + 1
-
-    # Extract hashtags with count higher than threshold
-    top_hashtags = set(tag for tag, count in hashtag_count.items() if count > threshold)
-
-    id2word = {i: tag for i, tag in enumerate(sorted(top_hashtags))}
-    word2id = {tag: i for i, tag in id2word.items()}
-
-    return id2word, word2id
-
-
-def count_cooccurence(tweets, word2id):
+def count_cooccurence(tag_table, word2id):
     """ Count the co-occurence of the hashtags.
 
     Parameters
@@ -102,9 +74,8 @@ def count_cooccurence(tweets, word2id):
 
     """
     co_occurence = dict()
-    for tweet in tweets:
-        matches = re.findall(r"#\w+[\w'-]*\w+", tweet.lower())
-        indices = sorted([word2id[word] for word in matches if word in word2id.keys()])
+    for tag_dict in tag_table:
+        indices = sorted([word2id[w] for w in tag_dict.keys() if w in word2id])
         for pair in combinations(indices, 2):
             co_occurence[pair] = co_occurence.get(pair, 0) + 1
 
